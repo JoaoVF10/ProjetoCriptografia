3611566b6f99f7a205bf5c0d68015589852e19b4
João Vítor Ferreira <jv96516609@gmail.com>
2025-08-21 16:42:47 -0300

    feat/ Adicionanado a criptografia do Boletim

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,55 @@
 package org.example;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import java.security.KeyPair;
+import java.util.Base64;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+
+
+        // Aqui a Escola irá gerar a chave para que depois os pais consigam acessar o boletim
+        SecretKey chaveAES = CriptoSimetrica.geraChave();
+        IvParameterSpec iv = CriptoSimetrica.geraIv();
+
+        //Escola tem a chave privada
+        KeyPair parChavesEscola = AssinaturaDigital.geraRSAKeyPair();
+
+        // Pais têm a chave pública
+        KeyPair parChavesPais = CriptoAssimetrica.geraRSAKeyPair();
+
+
+        String boletim = "Nota de Thalita: 10\nNota de João: 10\n Nota de Matheus: 10";
+        System.out.println("📄 Boletim :\n" + boletim);
+
+
+        // Boletim passa pelo processamento de encriptografia
+        byte[] boletimCriptografado = CriptoSimetrica.encripta(boletim, chaveAES, iv);
+
+
+        //criptografa a chave AES e a chave dos Pais
+        byte[] chaveAESCriptografada = CriptoAssimetrica.encripta(
+                Base64.getEncoder().encodeToString(chaveAES.getEncoded()),
+                parChavesPais.getPublic()
+        );
+
+
+        // Assina digitalmente o boletim
+        String assinaturaBase64 = AssinaturaDigital.assina(boletimCriptografado, parChavesEscola.getPrivate());
+
+
+        // Simulando os dados para envio
+        System.out.println("\n📦 Enviando boletim aos responsáveis...\n");
+
+        System.out.println("🔐 Boletim criptografado com AES:");
+        System.out.println(Base64.getEncoder().encodeToString(boletimCriptografado));
+
+        System.out.println("\n🔒 Chave AES criptografada com a chave pública dos pais:");
+        System.out.println(Base64.getEncoder().encodeToString(chaveAESCriptografada));
+
+        System.out.println("\n✍️ Assinatura digital feita com a chave privada da escola:");
+        System.out.println(assinaturaBase64);
+
     }
 }
